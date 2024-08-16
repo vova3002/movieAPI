bc4834f1a7754fe7d86a68b5e36cc124b3dce303
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-08-16 09:15:00 +0300

    done

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 // Отримати дані про фільм за допомогою OMDB API Опис: Використайте публічне API OMDB для отримання даних про фільм.Зробіть GET - запит за адресою http://www.omdbapi.com/?apikey={API_KEY}&t={movie_title}, де {API_KEY} - ваш ключ API OMDB, а {movie_title} - назва фільму. Перегляньте отримані дані щодо фільму.
+// http://www.omdbapi.com/?t=Spiderman&y=2024
+
+let movieInputer = document.querySelector(".movie__inputer")
+const button = document.querySelector(".button__submitter");
+
+movieInputer.addEventListener("change", (e) => {
+  const movie = e.target.value
+  console.log(movie)
+  getMovie(movie).then((data) => {
+    console.log(data)
+    htmlMovie(data)
+  })
+})
 const movieList = document.querySelector(".movie__list")
-function getMovie(){
-  const data = fetch("http://www.omdbapi.com/?i=tt3896198&apikey=659ac179").then((returnData) => {
+function getMovie(movieInput) {
+  // console.log(movieInput)
+  const data = fetch(`https://www.omdbapi.com/?apikey=659ac179&t=${movieInput}`).then((returnData) => {
+    console.log(returnData)
     return returnData.json()
   })
   return data
 }
-getMovie().then((movieData) => {
-  console.log(movieData)
-  function html(){
-    const movieHtml = `    <li class="movie__item">
-    <img class="movie__image" src=${movieData.Poster}>
-    <p class="movie__actor">Actors: ${movieData.Actors}</p>
-    <p class="movie__awards">Awards: ${movieData.Awards}</p>
-    <p class="movie__country">Country: ${movieData.Country}</p>
-    <p class="movie__english">Language: ${movieData.Language}</p>
-    </li>`
-    return movieHtml
-  }
-  movieList.innerHTML = html()
-})
+
+const htmlMovie = (movieData) => {
+  const htmlmovies = `    <li class="movie__item">
+  <h1 class="movie__title">Title: ${movieData.Title}</h1>
+  <p class="movie__text">Rated:${movieData.Rated}</p>
+  <p class="movie__actors">Actors:${movieData.Actors}</p>
+  <p class="movie__country">Country: ${movieData.Country}</p>
+  <img class="movie__image" src=${movieData.Poster}>
+</li>`
+  movieList.innerHTML = htmlmovies
+}
